12c69af4c918a07104e77d1c52d7419be00ba59b
Robert Foy <robert.foy@gmail.com>
2022-12-25 06:15:08 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Valheim Instant Craft
+
+## Details
+
+To get this to compile and build with Visual Studio, you'll need to download some additional libraries.
+[This video guide](https://www.youtube.com/watch?v=p_gsFASlvRw) goes through the basics on how you can get set-up.
---
+++ ValheimMod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Valheim_Mod
+{
+    [BepInPlugin("rjf.ValheimMod", "Valheim Mod", "1.0.1")]
+    [BepInProcess("valheim.exe")]
+    public class ValheimMod : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("rjf.ValheimMod");
+
+        void Awake()
+        {
+            harmony.PatchAll();
+        }
+
+
+        [HarmonyPatch(typeof(InventoryGui), "UpdateRecipe")]
+        class Craft_Patch
+        {
+            static void Prefix(ref InventoryGui __instance)
+            {
+                __instance.m_craftDuration = .01f;
+            }
+        }
+    }
+}
